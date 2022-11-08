4e0ba0112d703744fc2fdd02bc4eb0e70d73e518
anggelasellap <anggelasellap8@gmail.com>
2022-11-08 20:05:00 +0700

    anggelasellap8

---
+++ about.php
@@ -0,0 +1,72 @@
+<?php
+  include('koneksi.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title> About </title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+  <!-- Navbar -->
+
+  <!-- Main Sidebar Container -->
+  <?php
+    include('sidebar.php');
+  ?>
+   ?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>About</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item active"><a href="index.php"></a></li>
+              <!-- <li class="breadcrumb-item active">Blank Page</li> -->
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+    <html>
+<head>
+<title>Membuat Biodata Sederhana</title>
+</head>
+<body>
+<form action="#" style="width: 700px"class="posisi";>
+<fieldset class="h"/>
+<table style="width: 600px;">
+<body style="background-color:white"> </body>
+<tr>
+<td rowspan="10" width="100px">
+<img src="foto.jpg" width="150px" height="200px" border="50px">
+</td>
+</tr>
+<tr>
+<td>Aplikasi ini dibuat oleh :</td>
+</tr>
+<tr>
+<td>Nama : Anggela Sella Paat</td>
+<tr>
+<td>NIM : 2031730081</td>
+<tr>
+<td>Tanggal : 05-November-2022</td>
+</tr>
+</body>
+</html>
